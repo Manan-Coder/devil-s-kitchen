6cd64af1ae4d73982b6654cc95e12d9359ead147
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-02-11 13:59:32 +0530

    added a custom camera limit for every scene

---
+++ scenes/home.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+func _ready():
+	SceneManager.set_camera_limits("home", 4, 911, 0, 335)
---
+++ scenes/way_1.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+func _ready():
+	SceneManager.set_camera_limits("way1", 2, 2000, 0, 395)
