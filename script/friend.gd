47239ccffd1332f8e230df00e52852fee0f90a0e
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-02-12 16:55:36 +0530

    made scene transitions for the first scene

--- script/friend.gd
+++ script/friend.gd
@@ -1,68 +1,68 @@
-extends CharacterBody2D
-
-const speed = 100
-var current_dir = "none"
-
+extends CharacterBody2D 
+#
+#const speed = 100
+#var current_dir = "none"
+#
 func _ready():
 	$AnimatedSprite2D.play("front-idle")
-
-func _physics_process(delta):
-	player_movement(delta)
-
-func player_movement(delta):
-	if Input.is_action_pressed("ui_right"):
-		velocity.x = speed
-		velocity.y = 0
-		current_dir = "right"
-		play_anim(1)
-	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -speed
-		velocity.y = 0
-		current_dir = "left"
-		play_anim(1)
-	elif Input.is_action_pressed("ui_down"):
-		velocity.x = 0
-		velocity.y = speed
-		current_dir = "down"
-		play_anim(1)
-	elif Input.is_action_pressed("ui_up"):
-		velocity.x = 0
-		velocity.y = -speed
-		current_dir = "up"
-		play_anim(1)
-	else:
-		velocity.x = 0
-		velocity.y = 0
-		play_anim(0)
-		
-	move_and_slide()
-
-func play_anim(movement):
-	var dir = current_dir
-	var anim = $AnimatedSprite2D
-	
-	if dir == "right":
-		anim.flip_h = false
-		if movement == 1:
-			anim.play("side-walk")
-		elif movement == 0:
-			anim.play("side-idle")
-	if dir == "left":
-		anim.flip_h = true
-		if movement == 1:
-			anim.play("side-walk")
-		elif movement == 0:
-			anim.play("side-idle")
-		
-	if dir == "down":
-		anim.flip_h = false
-		if movement == 1:
-			anim.play("front-walk")
-		elif movement == 0:
-			anim.play("front-idle")
-	if dir == "up":
-		anim.flip_h = true
-		if movement == 1:
-			anim.play("back-walk")
-		elif movement == 0:
-			anim.play("back-idle")
+#
+#func _physics_process(delta):
+	#player_movement(delta)
+#
+#func player_movement(delta):
+	#if Input.is_action_pressed("ui_right"):
+		#velocity.x = speed
+		#velocity.y = 0
+		#current_dir = "right"
+		#play_anim(1)
+	#elif Input.is_action_pressed("ui_left"):
+		#velocity.x = -speed
+		#velocity.y = 0
+		#current_dir = "left"
+		#play_anim(1)
+	#elif Input.is_action_pressed("ui_down"):
+		#velocity.x = 0
+		#velocity.y = speed
+		#current_dir = "down"
+		#play_anim(1)
+	#elif Input.is_action_pressed("ui_up"):
+		#velocity.x = 0
+		#velocity.y = -speed
+		#current_dir = "up"
+		#play_anim(1)
+	#else:
+		#velocity.x = 0
+		#velocity.y = 0
+		#play_anim(0)
+		#
+	#move_and_slide()
+#
+#func play_anim(movement):
+	#var dir = current_dir
+	#var anim = $AnimatedSprite2D
+	#
+	#if dir == "right":
+		#anim.flip_h = false
+		#if movement == 1:
+			#anim.play("side-walk")
+		#elif movement == 0:
+			#anim.play("side-idle")
+	#if dir == "left":
+		#anim.flip_h = true
+		#if movement == 1:
+			#anim.play("side-walk")
+		#elif movement == 0:
+			#anim.play("side-idle")
+		#
+	#if dir == "down":
+		#anim.flip_h = false
+		#if movement == 1:
+			#anim.play("front-walk")
+		#elif movement == 0:
+			#anim.play("front-idle")
+	#if dir == "up":
+		#anim.flip_h = true
+		#if movement == 1:
+			#anim.play("back-walk")
+		#elif movement == 0:
+			#anim.play("back-idle")
--- script/global.gd
+++ script/global.gd
@@ -1,9 +1,4 @@
 extends Node
 
 var current_scene = "home"
-var transitoin_scene = false
-
-var player_exitx = 0
-var player_exity = 0
-var player_startx = 0
-var player_starty = 0
+var transtition_scene = false
