f50e01465c9397a1357f4e6ad70a3c2dfba2339d
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-03-18 01:41:59 +0530

    Made a friend falling animation

--- script/global.gd
+++ script/global.gd
@@ -20,5 +20,5 @@ var friend_interaction_cutscene = 0
 var friend_interaction_friend_pause = 0
 var potion_active = false
 var gun_got = false
-var make_spidey = true
+var make_spidey = false
 var boars_killed = 0
